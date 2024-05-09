5ddf5be4b06aa992cdf338cb277d0cf73da52fe1
Till Beemelmanns <till.beemelmanns@gmail.com>
2024-05-09 21:22:28 +0000

    make compatible with mmdetection3d v1.4.0

--- converter/img_converter.py
+++ converter/img_converter.py
@@ -12,6 +12,27 @@ import img
 IMG_CORRUPTIONS = ['snow', 'fog', 'temporalmisalignment', 'brightness', 'dark',
                    'missingcamera', 'motionblur']
 
+def update_pathes_dev1x(infos):
+    """
+    For .pkl files from mmdetection3d framework on branch dev-1.x
+    Add 'samples' and 'CAM_FRONT' '...' to the pathes.
+    """
+    for info in infos:
+        for k, v in info['images'].items():
+            info['images'][k]['img_path'] = os.path.join('samples', k, v['img_path'])
+    return infos
+
+def update_pathes(infos):
+    """
+    For .pkl files from mmdetection3d framework older than 1.x.
+    Remove the first 16 characters from the data_path.
+    """
+    for info in infos:
+        for k, v in info['cams'].items():
+            info['cams'][k]['data_path'] = v['data_path'][16:]
+    return infos
+
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Generate corrupted nuScenes dataset for image data')
     parser.add_argument('-c', '--n_cpus', help='number of CPUs that should be used', type=int,
@@ -38,42 +59,48 @@ if __name__ == '__main__':
     print(f'using {args.seed} as numpy random seed')
     np.random.seed(args.seed)
 
-    imageset = os.path.join(args.root_folder, "deepinter_infos_val.pkl")
+    imageset = os.path.join(args.root_folder, "nuscenes_infos_val.pkl")
 
     with open(imageset, 'rb') as f:
         infos = pickle.load(f)
 
-    all_files = infos['infos']
-    all_paths = copy.deepcopy(all_files)
-
+    if 'infos' in infos:
+        all_files = infos['infos']
+        IMG_KEY = 'cams'
+        FILE_KEY = 'data_path'
+        all_files = update_pathes(all_files)
+    elif 'data_list' in infos:
+        all_files = infos['data_list']
+        IMG_KEY = 'images'
+        FILE_KEY = 'img_path'
+        all_files = update_pathes_dev1x(all_files)
+    else:
+        exit("This mmdetection3d version is not supported.")
+    
     Path(args.dst_folder).mkdir(parents=True, exist_ok=True)
-    lidar_save_root = os.path.join(args.dst_folder, 'samples/LIDAR_TOP')
-    if not os.path.exists(lidar_save_root):
-        os.makedirs(lidar_save_root)
 
     def _map(i: int) -> None:
-        info = all_paths[i]
-        data_paths = [cam_info['data_path'] for cam_info in info['cams'].values()]
+        info = all_files[i]
+        data_paths = [cam_info[FILE_KEY] for cam_info in info[IMG_KEY].values()]
 
         if args.corruption == 'temporalmisalignment':
             s = [0.2, 0.4, 0.6][args.severity - 1]
             for j in range(6):
                 if np.random.rand() < s and i>=1:
-                    prev_info = all_paths[i-1]
-                    prev_data_paths = [cam_info['data_path'] for cam_info in prev_info['cams'].values()]
-                    new_data_path = prev_data_paths[j][16:] 
+                    prev_info = all_files[i-1]
+                    prev_data_paths = [cam_info[FILE_KEY] for cam_info in prev_info[IMG_KEY].values()]
+                    new_data_path = prev_data_paths[j]
                 else:
-                    new_data_path = data_paths[j][16:]
+                    new_data_path = data_paths[j]
 
                 full_input_path = os.path.join(args.root_folder, new_data_path)
-                full_output_path = os.path.join(args.dst_folder, data_paths[j][16:])
+                full_output_path = os.path.join(args.dst_folder, data_paths[j])
                 
                 os.makedirs(os.path.dirname(full_output_path), exist_ok=True)
                 cv2.imwrite(full_output_path, cv2.imread(full_input_path, cv2.IMREAD_UNCHANGED))
         else:
             for path in data_paths:
-                new_data_path = path[16:]
-                image = cv2.imread((os.path.join(args.root_folder, new_data_path)), cv2.IMREAD_UNCHANGED)
+                image = cv2.imread((os.path.join(args.root_folder, path)), cv2.IMREAD_UNCHANGED)
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
                 if args.corruption == 'snow':
@@ -102,8 +129,8 @@ if __name__ == '__main__':
                     raise NotImplementedError('Corruption not implemented')
 
                 new_image = new_image.astype(np.uint8)
-                os.makedirs(os.path.dirname(os.path.join(args.dst_folder, new_data_path)), exist_ok=True)
-                cv2.imwrite(os.path.join(args.dst_folder, new_data_path), cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR))
+                os.makedirs(os.path.dirname(os.path.join(args.dst_folder, path)), exist_ok=True)
+                cv2.imwrite(os.path.join(args.dst_folder, path), cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR))
 
     n_files = len(all_files)
     with mp.Pool(processes=args.n_cpus) as pool:
